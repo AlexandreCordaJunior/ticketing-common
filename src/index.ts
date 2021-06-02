fb80a1a945bb6ae3b54e4efd03df16ef7213e1c9
Alexandre Corda <alexandre.corda@fatec.sp.gov.br>
2021-06-01 21:33:02 -0300

    teste

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+const color: Color = {
+    red: 15,
+    blue: 45,
+    green: 50
+};
+
+console.log(color);
+
+export default color;
